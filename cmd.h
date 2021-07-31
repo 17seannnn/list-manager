2327e235454e77527e3f090994a849831b22c255
17sean <17sean@protonmail.com>
2021-07-31 17:00:53 +0300

    refactor: move code in cmd.c/cmd.h

--- cmd.h
+++ cmd.h
@@ -1,4 +1,20 @@
 #ifndef CMD_H
 #define CMD_H
 
+enum command {
+        cmd_nothing,
+        cmd_help,
+        cmd_quit,
+        cmd_chmod,
+        cmd_chcur,
+        cmd_add,
+        cmd_dsp,
+        cmd_dsp_cur,
+        cmd_show,
+        cmd_search
+};
+
+int parse_cmd(enum command *cmd, int *val);
+int handle_cmd(enum command cmd, int val, struct pointer *p, enum mode *m);
+
 #endif
