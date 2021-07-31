79c935b4728efae2d6cb4b837e12bf6070c43ae4
17sean <17sean@protonmail.com>
2021-07-31 15:39:13 +0300

    refactor: add & move code in macrofunc_list.h

---
+++ macrofunc_list.h
@@ -0,0 +1,38 @@
+#ifndef MACROFUNC_LIST_H
+#define MACROFUNC_LIST_H
+
+#define MAKE_DISPOSE_LIST_FUNCTION(MODE) \
+        void dsp_ ## MODE(struct MODE ## _item *first) \
+        { \
+                struct MODE ## _item *tmp; \
+                while (first) { \
+                        tmp = first; \
+                        first = first->next; \
+                        free(tmp); \
+                } \
+        }
+
+#define MAKE_SHOW_LIST_FUNCTION(MODE) \
+        void show_ ## MODE(struct MODE ## _item *first, \
+                           struct MODE ## _item *cur) \
+        { \
+                for (; first; first = first->next) \
+                        printf("%d %c\n", \
+                                first->data, first == cur ? '*' : 0); \
+        }
+
+#define MAKE_SEARCH_LIST_FUNCTION(MODE) \
+        void search_ ## MODE(struct MODE ## _item *first, \
+                             struct MODE ## _item **pcur, \
+                             int n) \
+        { \
+                for (; first; first = first->next) \
+                        if (n == first->data) { \
+                                printf("Found.\n"); \
+                                *pcur = first; \
+                                return; \
+                        } \
+                printf("Not found.\n"); \
+        }
+
+#endif
--- main.c
+++ main.c
@@ -31,41 +31,8 @@
 #include "option.h"
 #include "mode.h"
 #include "help.h"
+#include "macrofunc_list.h"
 #include "error.h"
-
-#define MAKE_DISPOSE_LIST_FUNCTION(MODE) \
-        void dsp_ ## MODE(struct MODE ## _item *first) \
-        { \
-                struct MODE ## _item *tmp; \
-                while (first) { \
-                        tmp = first; \
-                        first = first->next; \
-                        free(tmp); \
-                } \
-        }
-
-#define MAKE_SHOW_LIST_FUNCTION(MODE) \
-        void show_ ## MODE(struct MODE ## _item *first, \
-                           struct MODE ## _item *cur) \
-        { \
-                for (; first; first = first->next) \
-                        printf("%d %c\n", \
-                                first->data, first == cur ? '*' : 0); \
-        }
-
-#define MAKE_SEARCH_LIST_FUNCTION(MODE) \
-        void search_ ## MODE(struct MODE ## _item *first, \
-                             struct MODE ## _item **pcur, \
-                             int n) \
-        { \
-                for (; first; first = first->next) \
-                        if (n == first->data) { \
-                                printf("Found.\n"); \
-                                *pcur = first; \
-                                return; \
-                        } \
-                printf("Not found.\n"); \
-        }
 
 enum command {
         cmd_nothing,
