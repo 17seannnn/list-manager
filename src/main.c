904e5e5aae4ed712f0e34c3e3f6f55e23513daf1
17sean <17sean@protonmail.com>
2021-08-01 17:21:33 +0300

    docs: remove todo from main.c

--- src/main.c
+++ src/main.c
@@ -1,30 +1,3 @@
-/*
- *                                   TODO
- * Now:
- *
- * Divide code on parts *
- * Add info to Install in README
- *
- * Later:
- * Add show cur cmd, change search on '?'
- * Delete change_mode func and use parse_mode for when cmd_chmod
- * Use my functions for operations with strings
- *
- * Add in current prev or next pointer for single&doubly-linked lists
- * Add_cur, search current also change current pointer
- *
- * Remove value parsing from parse_cmd, add parse_value
- * Delete node: user enter the value and we delete node with this value
- * Improve README
- *
- * Much later:
- * Change chcur func:
- *      1) If cur is NULL then in case n is 'N'/'n' we set cur ptr on first ptr,
- *              in case n is 'P'/'p' we set cur ptr on last ptr.
- *      2) If next/prev ptr is NULL, then set cur on NULL.
- *      3) int -> void chcur_single(), remove res = chcur_single()
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
