5d14be3bf43a8785ed1a77b69fb54ee93362b4e3
17sean <17sean@protonmail.com>
2021-08-13 20:18:48 +0300

    refactor: move TODO & build.sh

--- src/build.sh
+++
@@ -1,4 +0,0 @@
-CC=gcc
-
-$CC -ansi -pedantic -Wall -Og -g -c option.c mode.c cmd.c help.c single.c doubly.c bintree.c dsp_all.c lm_string.c value.c version.c
-$CC -ansi -pedantic -Wall -Og -g main.c option.o mode.o cmd.o help.o single.o doubly.o bintree.o dsp_all.o lm_string.o value.o version.o -o lm
