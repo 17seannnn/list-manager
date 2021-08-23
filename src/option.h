c9483241e2f8b105f8a75834b70d76d50aef93fd
17sean <17sean@protonmail.com>
2021-08-23 16:48:21 +0300

    refactor: move all options in option.h

--- src/option.h
+++ src/option.h
@@ -1,6 +1,12 @@
 #ifndef OPTION_H
 #define OPTION_H
 
+#define OPTION_HELP_SHORT "-h"
+#define OPTION_HELP_LONG "--help"
+
+#define OPTION_VERSION_SHORT "-V"
+#define OPTION_VERSION_LONG "--version"
+
 int handle_opt(char **argv);
 
 #endif
--- src/version.h
+++ src/version.h
@@ -17,9 +17,6 @@
 #define AUTHOR_NICKNAME "17sean"
 #define AUTHOR_PAGE "https://github.com/17sean"
 
-#define OPTION_VERSION_SHORT "-V"
-#define OPTION_VERSION_LONG "--version"
-
 void version_opt();
 
 #endif
