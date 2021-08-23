23b217a24cad37d033e8c65d86bfd2508f0d9d37
17sean <17sean@protonmail.com>
2021-08-23 18:18:10 +0300

    build: rename files in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 CC=gcc
 
 cd src/
-$CC -ansi -pedantic -Wall -Og -g -c error.c option.c mode.c cmd.c help.c single.c doubly.c bintree.c dsp_all.c lm_string.c value.c version.c
-$CC -ansi -pedantic -Wall -Og -g main.c error.o option.o mode.o cmd.o help.o single.o doubly.o bintree.o dsp_all.o lm_string.o value.o version.o -o ../lm
+$CC -ansi -pedantic -Wall -Og -g -c error.c option.c mode.c cmd.c help.c single.c doubly.c bintree.c dsp_all.c lmstring.c value.c version.c
+$CC -ansi -pedantic -Wall -Og -g main.c error.o option.o mode.o cmd.o help.o single.o doubly.o bintree.o dsp_all.o lmstring.o value.o version.o -o ../lm
 mv -f ../lm /usr/bin
