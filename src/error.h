7d06c26005e2084d86dacc4d824f9e49ea0164fb
17sean <17sean@protonmail.com>
2021-08-23 18:57:31 +0300

    docs: improve translation, add info in po file

--- src/error.h
+++ src/error.h
@@ -1,9 +1,11 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+#include "gtmd.h"
+
 #define ERR_EOF 1
 
-#define ERR_EOF_TEXT "error: return expected but end of file found\n"
+#define ERR_EOF_TEXT _("error: return expected but end of file found\n")
 
 void printerr(int err);
 
