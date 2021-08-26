61f1a739edaf4fb093763fe0fb6da2a397e3dba2
17sean <17sean@protonmail.com>
2021-08-26 17:44:10 +0300

    fix!: move binary file to local bin dir

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ case $1 in
                 $CC -ansi -pedantic -Wall -Og -g main.c error.o option.o \
                     mode.o cmd.o help.o single.o doubly.o bintree.o \
                     dsp_all.o lmstring.o value.o version.o -o ../lm;
-                mv -f ../lm /usr/bin/
+                mv -f ../lm /usr/local/bin/
                 cd ../po/;
                 mkdir -p /usr/share/locale/ru/LC_MESSAGES;
                 msgfmt ru.po -o /usr/share/locale/ru/LC_MESSAGES/lm.mo;;
